ee65974b2092370a08801b999b2ede0593d97bc4
Scott Andrew Martin <samart18@pfe24.nas.nasa.gov>
2025-03-24 17:31:21 -0700

    podaac fix

--- src/downloaders.py
+++ src/downloaders.py
@@ -4,13 +4,11 @@ import sys
 from glob import glob
 import copernicusmarine as cm
 import xarray as xr
-# cm.login()
+cm.login()
 
 # #############################################
 # # MUR SST
 # #############################################
-
-remove_mur_nc = True
 
 
 def format_date_to_string(date_object):
@@ -34,13 +32,14 @@ date_end_input = sys.argv[2]
 date_start = datetime.strptime(date_start_input, "%Y-%m-%d").date()
 date_end = datetime.strptime(date_end_input, "%Y-%m-%d").date()
 
+
 start_date_string = format_date_to_string(date_start)
 end_date_string = format_date_to_string(date_end)
 
 n_days = (date_end-date_start).days
 for t in range(n_days):
 
-    command = 'podaac-data-downloader -c MUR-JPL-L4-GLOB-v4.1 -d ./input_data/mur_sst_tmp --start-date '+format_date_to_string(date_start+timedelta(days=t))+' --end-date '+format_date_to_string(date_start+timedelta(days=t+1))+' -b="-180,-90,180,90"'
+    command = 'podaac-data-downloader -c MUR-JPL-L4-GLOB-v4.1 -d ./input_data/mur_sst_tmp --start-date '+format_date_to_string(date_start+timedelta(days=t))+' --end-date '+format_date_to_string(date_start+timedelta(days=t))+' -b="-180,-90,180,90"'
     
     os.system(command)
     
@@ -67,31 +66,31 @@ for t in range(n_days):
 #############################################
 
 
-# sats = ['al','c2n','h2b','j3n','s3a','s3b','s6a-hr','swon']
+sats = ['al','c2n','h2b','j3n','s3a','s3b','s6a-hr','swon']
 
 
-# def generate_date_list(start_date, end_date):
-#     date_list = ['*/'+(start_date + timedelta(days=i)).strftime('%Y/%m') +'/*.nc' for i in range((end_date - start_date).days + 1)]
-#     return list(set(date_list))
+def generate_date_list(start_date, end_date):
+    date_list = ['*/'+(start_date + timedelta(days=i)).strftime('%Y/%m') +'/*.nc' for i in range((end_date - start_date).days + 1)]
+    return list(set(date_list))
 
-# date_start_input = sys.argv[1]
-# date_end_input = sys.argv[2]
+date_start_input = sys.argv[1]
+date_end_input = sys.argv[2]
 
-# # def download_mur_local(date_start_input,date_end_input):
-# date_start = datetime.strptime(date_start_input, "%Y-%m-%d").date()
-# date_end = datetime.strptime(date_end_input, "%Y-%m-%d").date()
+# def download_mur_local(date_start_input,date_end_input):
+date_start = datetime.strptime(date_start_input, "%Y-%m-%d").date()
+date_end = datetime.strptime(date_end_input, "%Y-%m-%d").date()
 
 
-# search_strings = generate_date_list(date_start, date_end)
+search_strings = generate_date_list(date_start, date_end)
 
-# for sat in sats:
-#     print('Downloading data for '+sat)
-#     for search in search_strings:
-#         print(search)
-#         cm.get(
-#             dataset_id = "cmems_obs-sl_glo_phy-ssh_nrt_"+sat+"-l3-duacs_PT1S",
-#             filter = search,
-#             output_directory = 'input_data/cmems_sla/'+sat+'/',
-#             no_directories = True,
-#             force_download = True
-#         )
+for sat in sats:
+    print('Downloading data for '+sat)
+    for search in search_strings:
+        print(search)
+        cm.get(
+            dataset_id = "cmems_obs-sl_glo_phy-ssh_nrt_"+sat+"-l3-duacs_PT1S",
+            filter = search,
+            output_directory = 'input_data/cmems_sla/'+sat+'/',
+            no_directories = True,
+            force_download = True
+        )
