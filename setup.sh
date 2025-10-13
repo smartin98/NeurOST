6b29346c4f0c1a2ea6a7975a092e5c29a0f94702
smartin98 <s98amartin@gmail.com>
2025-10-13 12:09:15 -0700

    fix download filenames in setup.sh

--- setup.sh
+++ setup.sh
@@ -13,10 +13,11 @@ wget https://zenodo.org/records/15116366/files/land_water_mask_10grid.nc
 wget https://zenodo.org/records/15116366/files/mdt_currents_0.1degree_grid.nc
 wget https://zenodo.org/records/15116366/files/mdt_hybrid_cnes_cls18_cmems2020_global.nc
 cd ..
-wget https://dataverse.harvard.edu/api/access/datafile/8111925
+wget https://dataverse.harvard.edu/api/access/datafile/8111925 -O coord_grids.npy
 mkdir model_weights
 cd model_weights
-wget https://dataverse.harvard.edu/api/access/datafile/8131431
-wget https://dataverse.harvard.edu/api/access/datafile/8131432
+wget https://dataverse.harvard.edu/api/access/datafile/8131431 -O 
+simvp_ssh_sst_ns1000000global_weights_epoch46
+wget https://dataverse.harvard.edu/api/access/datafile/8131432 -O simvp_ssh_ns1000000_global_weights_epoch45
 cd ..
 cd ..
